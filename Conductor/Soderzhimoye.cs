550225ac22b4b3b30dc039d7f2742738d0500fda
AkaroDippens <112818299+AkaroDippens@users.noreply.github.com>
2022-12-10 22:56:01 +0300

    Add files via upload

---
+++ Conductor/Soderzhimoye.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conductor
+{
+    internal static class Soderzhimoye
+    {
+        
+        public static string Nachalo()
+        {
+            Console.SetCursorPosition(50, 0);
+            Console.WriteLine("Этот компьютер");
+            Console.SetCursorPosition(0, 1);
+            Console.WriteLine("-------------------------------------------------------------------------------------------------");
+            DriveInfo[] diski = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in diski)
+            { 
+                Console.WriteLine($"  {drive.Name} Свободно {drive.TotalFreeSpace / 1073741824} ГБ из {drive.TotalSize / 1073741824} ГБ");                   
+            }
+            Strelki menu = new Strelki(2, 2);
+            int position = menu.Arrow() - 3;
+            return diski[position].Name;
+        }
+        public static string Catalog(string poc)
+        {
+            Console.SetCursorPosition(50, 0);
+            Console.WriteLine("Папка");
+            Console.WriteLine("  Название\t\t\t\t\t\t\t\t\t     Дата создания");
+            string pac = $"{poc}";
+            List<string> elements = new List<string>();
+            var dirInfo = new DirectoryInfo(pac);  
+            int num = 0;
+            string[] direct = Directory.GetDirectories(pac);
+            string[] files = Directory.GetFiles(pac);
+            elements = [direct, files];
+            foreach (string elik in elements)
+            {
+                Console.WriteLine($"  {elik}");
+                Console.SetCursorPosition(80, num + 3);
+                Console.WriteLine($"  {dirInfo.CreationTime}");
+                num++;
+            }
+            Strelki menu = new Strelki(num + 3, 3);
+            int vybrpos = menu.Arrow()-3;
+            return Catalog(elements[vybrpos]);
+        }
+        public static void Zapusk()
+        {  
+            string drive = Nachalo();
+            string path = Catalog(drive);
+            Process.Start(new ProcessStartInfo {FileName = path, UseShellExecute = true});
+        }
+    }
+}
+
+
+
+
+
+
+                
+               
+        
+       
+                        
+                
+        
+    
+
+
+   
+
---
+++ Conductor/Strelki.cs
@@ -0,0 +1,50 @@
+﻿namespace Conductor
+{
+    internal class Strelki
+    {
+        int min, max;
+        ConsoleKey key;
+        int pos;
+        public Strelki(int max, int min = 0)
+        {
+           this.min = min;
+           this.max = max - 1;         
+        }
+        public int Arrow(int startPos = 0, int count = 3, string arrow = "->")
+        {    
+            string empty = new string(' ', arrow.Length);
+            int i = startPos;
+            Console.SetCursorPosition(0, startPos);
+            Console.Write(arrow);
+            ConsoleKeyInfo key;
+            for (; ; )
+            {
+                key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        if (i == count + startPos - 1)
+                            continue;
+                        Console.SetCursorPosition(0, i);
+                        Console.Write(empty);
+                        Console.SetCursorPosition(0, ++i);
+                        Console.Write(arrow);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        if (i == startPos)
+                            continue;
+                        Console.SetCursorPosition(0, i);
+                        Console.Write(empty);
+                        Console.SetCursorPosition(0, --i);
+                        Console.Write(arrow);
+                        break;
+                    case ConsoleKey.Enter:
+                        return i + 1 - startPos;
+                }
+            }
+        }
+    }   
+}
+
+
+
